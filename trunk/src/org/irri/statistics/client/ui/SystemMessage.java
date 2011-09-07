8f9fd9036ab8ff289b0bf1f899374234cb4b3a1a
jaunario@gmail.com <jaunario@gmail.com@189a1a5e-35c0-c4be-363c-3e9fc2bf592e>
2011-09-07 10:49:13 +0000

    Added SystemMessage (PopupPanel for important messages), Implemented changing some charts using menus.

---
+++ trunk/src/org/irri/statistics/client/ui/SystemMessage.java
@@ -0,0 +1,13 @@
+package org.irri.statistics.client.ui;
+
+import com.google.gwt.user.client.ui.PopupPanel;
+
+public class SystemMessage extends PopupPanel {
+
+	public SystemMessage() {
+		super(true);
+		setGlassEnabled(true);
+		center();
+	}
+
+}
--- trunk/src/org/irri/statistics/client/ui/charts/MultiChartPanel.java
+++ trunk/src/org/irri/statistics/client/ui/charts/MultiChartPanel.java
@@ -1,5 +1,8 @@
 package org.irri.statistics.client.ui.charts;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.irri.statistics.client.utils.NumberUtils;
 import org.irri.statistics.client.utils.RPCUtils;
 
@@ -7,10 +10,11 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.Frame;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.MenuItem;
-import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.visualization.client.AbstractDataTable;
 import com.google.gwt.visualization.client.VisualizationUtils;
@@ -28,14 +32,22 @@ import com.google.gwt.user.client.ui.MenuItemSeparator;
 
 public class MultiChartPanel extends Composite {
 	AbstractDataTable basedata;
-	public int charttype1f = 0;
-	public int charttype2f = 0;
+	AbstractDataTable chart1fdata;
+	AbstractDataTable chart2fdata;
+	private int charttype1f = 0;
+	private int charttype2f = 0;
+	private int series = 0;
+	private int x = 1;
+	private int y = 2;
+	
 	public int[] numerics;
-	public int plotcol = 0;
+
 	private DeckPanel deckChartPager;
 	private VerticalPanel vpTablePage;
 	private VerticalPanel vp1FactorChart;
 	private VerticalPanel vp2FactorChart;
+	private MenuBar mbChart1Year;
+	private MenuBar mbChart1Region;
 	/**
 	 * @wbp.parser.constructor
 	 */
@@ -43,7 +55,7 @@ public class MultiChartPanel extends Composite {
 		initPanels();
 	}
 	public MultiChartPanel(String[][] data) {
-		numerics = NumberUtils.createIntSeries(2, data[0].length-2, 1);
+		numerics = NumberUtils.createIntSeries(2, data[0].length-1, 1);
 		basedata = ChartDataTable.create(data, numerics);
 		initPanels();
 		
@@ -72,83 +84,6 @@ public class MultiChartPanel extends Composite {
 		MenuBar menuBar = new MenuBar(true);
 		
 		MenuItem mntmTableOptions = new MenuItem("Table Options", false, menuBar);
-		mbTableOptions.addItem(mntmTableOptions);
-		MenuBar menuBar_1 = new MenuBar(true);
-		
-		MenuItem mntmChartOptions = new MenuItem("Chart Options", false, menuBar_1);
-		MenuBar menuBar_2 = new MenuBar(true);
-		
-		MenuItem mntmfactorChart = new MenuItem("1-Factor Chart", false, menuBar_2);
-		
-		MenuItem mntmPie = new MenuItem("Pie", false, (Command) null);
-		menuBar_2.addItem(mntmPie);
-		
-		MenuItem mntmMap = new MenuItem("Map", false, (Command) null);
-		menuBar_2.addItem(mntmMap);
-		
-		MenuItemSeparator separator = new MenuItemSeparator();
-		menuBar_2.addSeparator(separator);
-		MenuBar menuBar_4 = new MenuBar(true);
-		
-		MenuItem mntmYear = new MenuItem("Year", false, menuBar_4);
-		menuBar_2.addItem(mntmYear);
-		MenuBar menuBar_5 = new MenuBar(true);
-		
-		MenuItem mntmRegion = new MenuItem("Region", false, menuBar_5);
-		menuBar_2.addItem(mntmRegion);
-		menuBar_1.addItem(mntmfactorChart);
-		MenuBar menuBar_3 = new MenuBar(true);
-		
-		MenuItem mntmfactorChart_1 = new MenuItem("2-Factor Chart", false, menuBar_3);
-		MenuBar menuBar_8 = new MenuBar(true);
-		
-		MenuItem mntmChartType = new MenuItem("Chart Type", false, menuBar_8);
-		
-		MenuItem mntmLine = new MenuItem("Line", false, (Command) null);
-		menuBar_8.addItem(mntmLine);
-		
-		MenuItem mntmBar = new MenuItem("Bar", false, (Command) null);
-		menuBar_8.addItem(mntmBar);
-		
-		MenuItem mntmScatter = new MenuItem("Scatter", false, (Command) null);
-		menuBar_8.addItem(mntmScatter);
-		menuBar_3.addItem(mntmChartType);
-		
-		MenuItemSeparator separator_1 = new MenuItemSeparator();
-		menuBar_3.addSeparator(separator_1);
-		MenuBar menuBar_6 = new MenuBar(true);
-		
-		MenuItem mntmSetX = new MenuItem("Set X", false, menuBar_6);
-		menuBar_3.addItem(mntmSetX);
-		MenuBar menuBar_7 = new MenuBar(true);
-		
-		MenuItem mntmSetY = new MenuItem("Set Y", false, menuBar_7);
-		menuBar_3.addItem(mntmSetY);
-		menuBar_1.addItem(mntmfactorChart_1);
-		mbTableOptions.addItem(mntmChartOptions);
-		
-		MenuItem mntmRedraw = new MenuItem("Redraw", false, new Command() {
-			public void execute() {
-				drawTable();
-				switch (charttype1f) {
-				case 0:
-					drawMap(0, 2, 1, basedata.getValueString(0, 1));
-					break;
-
-				default:
-					break;
-				}
-				switch (charttype2f) {
-				case 0:
-					drawLineChart(1, 2, 0);
-					break;
-
-				default:
-					break;
-				}
-			}
-		});
-		mbTableOptions.addItem(mntmRedraw);
 		
 		MenuItem mntmDownload = new MenuItem("Download", false, new Command() {
 			public void execute() {
@@ -159,7 +94,6 @@ public class MultiChartPanel extends Composite {
 						
 						@Override
 						public void onSuccess(String result) {
-							// TODO Auto-generated method stub
 							Frame myframe = new Frame(result);
 							deckChartPager.add(myframe);
 						}
@@ -167,7 +101,11 @@ public class MultiChartPanel extends Composite {
 						@Override
 						public void onFailure(Throwable caught) {
 							// TODO Auto-generated method stub
-							
+							PopupPanel msg = new PopupPanel();
+							msg.add(new HTML("Operation failed. Please try Again. If the problem persists please contact us. (Indicate the variables chosen)"));
+							msg.setGlassEnabled(true);
+							msg.center();
+							msg.show();
 						}
 					};
 					RPCUtils.getService("mysqlservice").SaveCSV(ChartDataTable.csvData(basedata), downloadAsyncCallback);
@@ -179,15 +117,91 @@ public class MultiChartPanel extends Composite {
 				}
 			}
 		});
-		mbTableOptions.addItem(mntmDownload);
+		menuBar.addItem(mntmDownload);
+		mntmTableOptions.setVisible(false);
+		mbTableOptions.addItem(mntmTableOptions);
+		MenuBar mbChartClass = new MenuBar(true);
+		
+		MenuItem mntmChartOptions = new MenuItem("Chart Options", false, mbChartClass);
+		MenuBar mbChart1Type = new MenuBar(true);
+		
+		MenuItem mntm1factorChart = new MenuItem("1-Factor Chart", false, mbChart1Type);
+		
+		MenuItem mntmPie = new MenuItem("Pie", false, new Command() {
+			public void execute() {
+				charttype1f = 0;
+			}
+		});
+		mbChart1Type.addItem(mntmPie);
+		
+		MenuItem mntmMap = new MenuItem("Map", false, new Command() {
+			public void execute() {
+				charttype1f = 1;
+				draw1fChart();
+			}
+		});
+		mbChart1Type.addItem(mntmMap);
+		
+		MenuItemSeparator separator = new MenuItemSeparator();
+		mbChart1Type.addSeparator(separator);
+		mbChart1Year = new MenuBar(true);
+		
+		MenuItem mntmYear = new MenuItem("Year", false, mbChart1Year);
+		mbChart1Type.addItem(mntmYear);
+		mbChart1Region = new MenuBar(true);
+		
+		MenuItem mntmRegion = new MenuItem("Region", false, mbChart1Region);
+		mbChart1Type.addItem(mntmRegion);
+		mbChartClass.addItem(mntm1factorChart);
+		MenuBar mbChart2Type = new MenuBar(true);
+		
+		MenuItem mntm2factorChart = new MenuItem("2-Factor Chart", false, mbChart2Type);
+		MenuBar menuBar_8 = new MenuBar(true);
+		
+		MenuItem mntmChartType = new MenuItem("Chart Type", false, menuBar_8);
+		
+		MenuItem mntmLine = new MenuItem("Line", false, new Command() {
+			public void execute() {
+				charttype2f = 0;
+			}
+		});
+		menuBar_8.addItem(mntmLine);
+		
+		MenuItem mntmBar = new MenuItem("Bar", false, new Command() {
+			public void execute() {
+				charttype2f = 1;
+			}
+		});
+		menuBar_8.addItem(mntmBar);
+		
+		MenuItem mntmScatter = new MenuItem("Scatter", false, (Command) null);
+		menuBar_8.addItem(mntmScatter);
+		mbChart2Type.addItem(mntmChartType);
+		
+		MenuItemSeparator separator_1 = new MenuItemSeparator();
+		mbChart2Type.addSeparator(separator_1);
+		MenuBar mbChart2X = new MenuBar(true);
+		
+		MenuItem mntmSetX = new MenuItem("Set X", false, mbChart2X);
+		mbChart2Type.addItem(mntmSetX);
+		MenuBar mbChart2Y = new MenuBar(true);
+		
+		MenuItem mntmSetY = new MenuItem("Set Y", false, mbChart2Y);
+		mbChart2Type.addItem(mntmSetY);
+		mbChartClass.addItem(mntm2factorChart);
+		mbTableOptions.addItem(mntmChartOptions);
+		
+		MenuItem mntmRedraw = new MenuItem("Redraw", false, new Command() {
+			public void execute() {
+				drawTable();
+				draw1fChart();
+				draw2fChart();
+			}
+		});
+		mntmRedraw.setTitle("Refit charts/table into the panel");
+		mbTableOptions.addItem(mntmRedraw);
 		ChartsWrapper.add(decoratedTabBar, DockPanel.SOUTH);
 		decoratedTabBar.setWidth("100%");
-		
-		VerticalPanel verticalPanel = new VerticalPanel();
-		ChartsWrapper.add(verticalPanel, DockPanel.EAST);
-		ChartsWrapper.setCellHeight(verticalPanel, "100%");
-		ChartsWrapper.setCellWidth(verticalPanel, "35%");
-		verticalPanel.setSize("100%", "100%");
 		
 		deckChartPager = new DeckPanel();
 		ChartsWrapper.add(deckChartPager, DockPanel.CENTER);
@@ -216,14 +230,40 @@ public class MultiChartPanel extends Composite {
 	}
 	
 	public void setBaseData(String[][] data){
-		numerics = NumberUtils.createIntSeries(2, data[0].length-2, 1);
+		numerics = NumberUtils.createIntSeries(2, data[0].length-1, 1);
 		basedata = ChartDataTable.create(data, numerics);
+		chart1fdata = ChartDataTable.filteredTable(basedata, series, y, x, basedata.getValueString(series, x));
+		chart2fdata = ChartDataTable.dataIntoSeries(basedata, x, y, series, true);
+		populateChart1Yr();
 		if(vp1FactorChart.getWidgetCount()>0) vp1FactorChart.clear();
 		if(vp2FactorChart.getWidgetCount()>0) vp2FactorChart.clear();
 		drawTable();
-		drawLineChart(1,2,0);
-		//drawPieChart(0, 2, 1, basedata.getValueString(0, 1));
-		drawMap(0, 2, 1, basedata.getValueString(0, 1));
+		draw1fChart();
+		draw2fChart();
+	}
+	
+	public void draw1fChart(){
+		switch (charttype1f) {
+		case 1:
+			drawMap();
+			break;
+
+		default:
+			drawPieChart();
+			break;
+		}
+	}
+	
+	public void draw2fChart(){
+		switch (charttype2f) {
+		case 1:
+			
+			break;
+
+		default:
+			drawLineChart();
+			break;
+		}
 	}
 	
 	public void drawTable(){
@@ -232,51 +272,48 @@ public class MultiChartPanel extends Composite {
 			
 			@Override
 			public void run() {
-				Table table = new Table(basedata, setTableSize(vpTablePage.getOffsetWidth(), vpTablePage.getOffsetWidth()));
+				Table table = new Table(basedata, setTableSize(deckChartPager.getOffsetWidth(), deckChartPager.getOffsetHeight()));
 				vpTablePage.add(table);
 			}
 		};
 		VisualizationUtils.loadVisualizationApi(onLoadCallback, Table.PACKAGE);
 	};
 	
-	public void drawLineChart(int x, int y, int series){
+	public void drawLineChart(){
 		if (vp2FactorChart.getWidgetCount()>0) vp2FactorChart.clear();
-		final AbstractDataTable lcDataTable = ChartDataTable.dataIntoSeries(basedata, x, y, series, true);
 		Runnable onLoadCallback = new Runnable() {
 			
 			@Override
 			public void run() {
-				LineChart line = new LineChart(lcDataTable, createCoreChartOptions(vpTablePage.getOffsetWidth(), vpTablePage.getOffsetWidth()));
+				LineChart line = new LineChart(chart2fdata, createCoreChartOptions(deckChartPager.getOffsetWidth(), deckChartPager.getOffsetHeight()));
 				vp2FactorChart.add(line);
 			}
 		};
 		VisualizationUtils.loadVisualizationApi(onLoadCallback, LineChart.PACKAGE);
 	}
 	
-	public void drawPieChart(int x, int y, int filtercol, String filterval){
+	public void drawPieChart(){
 		if (vp1FactorChart.getWidgetCount()>0) vp1FactorChart.clear();
-		final AbstractDataTable pcDataTable = ChartDataTable.filteredTable(basedata, x, y, filtercol, filterval);
 		Runnable onLoadCallback = new Runnable() {
 			
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				PieChart pie = new PieChart(pcDataTable, createCoreChartOptions(vpTablePage.getOffsetWidth(), vpTablePage.getOffsetWidth()));
+				PieChart pie = new PieChart(chart1fdata, createCoreChartOptions(deckChartPager.getOffsetWidth(), deckChartPager.getOffsetHeight()));
 				vp1FactorChart.add(pie);
 			}
 		};
 		VisualizationUtils.loadVisualizationApi(onLoadCallback, PieChart.PACKAGE);
 	}
 	
-	public void drawMap(int loc, int val, int yr, String selectyr){
+	public void drawMap(){
 		if (vp1FactorChart.getWidgetCount()>0) vp1FactorChart.clear();
-		final AbstractDataTable gmDataTable = ChartDataTable.filteredTable(basedata, loc, val, yr, selectyr);
 		Runnable onLoadCallback = new Runnable() {
 			
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				GeoMap map = new GeoMap(gmDataTable, createMapOptions(vpTablePage.getOffsetWidth(), vpTablePage.getOffsetWidth()));
+				GeoMap map = new GeoMap(chart1fdata, createMapOptions(deckChartPager.getOffsetWidth(), deckChartPager.getOffsetHeight()));
 				vp1FactorChart.add(map);
 			}
 		};
@@ -285,37 +322,52 @@ public class MultiChartPanel extends Composite {
 	
 	public Options createCoreChartOptions(int w, int h){
 		Options options = Options.create();
-		options.setWidth(w);
-		options.setHeight(h);
+		options.setWidth(w-5);
+		options.setHeight(h-5);
+		options.set("is3D", "true");
 		return options;
 		
 	}
 	
 	public Table.Options setTableSize(int w, int h) {
     	Table.Options options = Table.Options.create();
-    	options.setHeight((h-10)+"px");
-    	options.setWidth((w-10)+"px");
+    	options.setHeight((h-5)+"px");
+    	options.setWidth((w-5)+"px");
     	options.setShowRowNumber(true);
     	return options;
     }
     
 	public GeoMap.Options createMapOptions(int w, int h){
 		GeoMap.Options options = GeoMap.Options.create();
-		options.setWidth(w);
-        options.setHeight(h);
+		options.setWidth(w-5);
+        options.setHeight(h-5);
         options.setShowLegend(true);
+        options.setShowZoomOut(true);
         options.setShowZoomOut(true);
         options.setDataMode(DataMode.REGIONS);
         options.setColors(0xDDDDDD,0xCE0000,0xFF9E00,0xF7D708,0x9CCF31);        
         return options;
 	}
-	public VerticalPanel getVpTablePage() {
-		return vpTablePage;
+
+	public MenuBar getMbChart1Year() {
+		return mbChart1Year;
 	}
-	public VerticalPanel getVp1FactorChart() {
-		return vp1FactorChart;
+	
+	private void populateChart1Yr(){
+		ArrayList<String> yrs = ChartDataTable.getUniqueColumnVals(basedata, 1);
+		Collections.sort(yrs);
+		for (int i = 0; i < yrs.size(); i++) {
+			mbChart1Year.addItem(yrs.get(i), new Command() {
+				
+				@Override
+				public void execute() {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+		}
 	}
-	public VerticalPanel getVp2FactorChart() {
-		return vp2FactorChart;
+	public MenuBar getMbChart1Region() {
+		return mbChart1Region;
 	}
 }
